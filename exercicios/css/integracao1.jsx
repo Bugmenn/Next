c07d26c4daa8b8a227fbf1b0fc1608439093e1e1
Bugmenn <gabrielbvanzuita@gmail.com>
2024-04-05 11:40:51 -0300

    deleted:    exercicios/css/integracao1.jsx 	modified:   exercicios/pages/_app.js 	new file:   exercicios/pages/css/integracao1.css 	new file:   exercicios/pages/css/integracao1.jsx

--- exercicios/css/integracao1.jsx
+++
@@ -1,9 +0,0 @@
-export default function integracao() {
-    return (
-        <>
-            <div className="vermelho">texto</div>
-            <div className="azul">texto</div>
-            <div className="branco">texto</div>
-        </>
-    )
-}
---
+++ exercicios/pages/css/integracao1.jsx
@@ -0,0 +1,15 @@
+// Para importar o css, deve importa-lo dentro do arquivo _app.js
+// Sendo essa importação global, ou seja, todos podem usar, assim causa o problema de utilização
+// portanto deve-se colocar especificações (se for filho) para não gerar problemas.
+
+import Titulo from "@/components/Titulo"
+
+export default function integracaoCSS() {
+    return (
+        <>
+            <div className="vermelho">texto</div>
+            <div className="azul">texto</div>
+            <div className="branco">texto</div>
+        </>
+    )
+}
